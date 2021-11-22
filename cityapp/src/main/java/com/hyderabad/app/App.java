c1d09df07da753fa134939c4c9269b0e5897c1d8
root <root@ip-172-31-10-60.ap-south-1.compute.internal>
2021-11-22 16:51:04 +0000

    hyderabadbranchadded

---
+++ cityapp/src/main/java/com/hyderabad/app/App.java
@@ -0,0 +1,13 @@
+package com.hyderabad.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hai Hyderabad. This is your fiest project. welcome" );
+    }
+}
